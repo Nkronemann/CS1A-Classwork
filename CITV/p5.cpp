ebea44277eccd58fe70e10f2c38a308c54330948
Nkronemann <nkronemann1@saddleback.edu>
2024-09-04 03:23:45 +0000

    added words tp p.5

--- CITV/p5.cpp
+++ CITV/p5.cpp
@@ -2,8 +2,9 @@
 using namespace std;
 int main() {
 int x,y;
+cout << "Enter a number:  ";
 cin >> x;
 y=2*x;
-cout << y << endl;
+cout << "Double your number: "<<  y << endl;
 }
 // Y is equal to 2 times the value inputted for x
