18a891204dc97c22148af8d673ccd0ee857e54c2
Nkronemann <nkronemann1@saddleback.edu>
2024-09-18 03:45:51 +0000

    91724 part 3 changes

--- Ifelseshort/p3.cpp
+++ Ifelseshort/p3.cpp
@@ -9,6 +9,8 @@ int main() {
     cin >> ang2;
     cin >> ang3;
    }
- if (ang1==90 or ang2==90 or ang3==90) cout << "its a right triangle" 
- else if 
+ if (ang1==90 or ang2==90 or ang3==90) cout << "its a right triangle" << endl;
+ else if (ang1<90 and ang2<90 and ang3<90) cout <<"This is an accute triangle" << endl;
+ else if (ang1>90 or ang2>90 or ang3>90) cout <<"This is an obtuse triangle" << endl;
+
 }
