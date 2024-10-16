6856ab859b5b2dc946e8b7446f0e69de274db23e
Nkronemann <nkronemann1@saddleback.edu>
2024-10-16 03:40:35 +0000

    10/15/24 Finale

--- Ass7/p2.cpp
+++ Ass7/p2.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
+int main() {
     string input;
     char A, Z, a, z;
     int uppercase, lowercase, digits, spaces;
     getline (cin, input);
+    uppercase=0; lowercase=0; digits=0; spaces=0;
 for (int i=0; i<input.length(); i++) {
     if (input[i]>=65 && input[i]<=90) {
         uppercase++;
@@ -14,7 +14,9 @@ for (int i=0; i<input.length(); i++) {
         lowercase++;
     } else if (input[i]>=1 && input[i]<=9) {
         digits++;
-    } else if (input[i]>=32 && input[i]<=32)
+    } else if (input[i]>=32 && input[i]<=32) {
+        spaces++;
+    }
 }
-
+cout << uppercase << ' ' << lowercase << ' ' << digits << ' ' << spaces << endl;
 }
