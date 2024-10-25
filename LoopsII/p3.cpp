c560ddf9ca2d56d06b9498302404b8a92282421b
Nkronemann <nkronemann1@saddleback.edu>
2024-10-25 02:51:56 +0000

    10/24/24 Finale

---
+++ LoopsII/p3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int x=0,y=0;
+cout << "Please input the height, and width of the grid you want to create\n";
+cin >> x;
+cin >> y;
+for (int i=0;i>0;) {
+for (x;x>0;) {
+cout << " * ";
+x--;
+when (x==0) 
+} 
+}
+}
