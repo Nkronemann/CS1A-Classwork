f45b2c0371b8ac02eba8409ba9ab6dbddc088d3c
Nkronemann <nkronemann1@saddleback.edu>
2024-12-02 05:24:53 +0000

    12/1 Add/Display Finished/working

---
+++ Finale/Finale.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+#include<fstream>
+
+using namespace std;
+//---------------------------------------------------//
+void Menu(){
+    // welcome message
+    cout << "Welcome to the Basic To-Do List Manager!\n";
+    //first 3
+    cout << "1. Add Task\n" << "2. Display Tasks\n" << "3. Remove Task\n";
+    //finale 4
+    cout << "4. Mark Task as Complete\n" << "5. Save to File\n" << "6. Load from File\n" << "7. Exit\n";
+    cout << endl << "Please Input the number corresponding to what action you would like to take\n";
+}
+//---------------------------------------------------//
+string AddTask (string ToDoArray[], int taskcount) {
+        cout << "Please input the name of a task you would like to add.\n";
+        cin.ignore();
+        getline(cin, ToDoArray[taskcount]);
+        cout << "Task has been added.\n";
+    return ToDoArray[taskcount];
+    }
+//---------------------------------------------------//
+void DisplayTask (string ToDoArray[], int taskcount) {
+    cout << "To-Do list:\n";
+    for (int i = 0; i < taskcount; i++) {
+        cout << i + 1 << ". " << ToDoArray[i] << endl;
+    }
+}
+//---------------------------------------------------//
+int main(){
+    //int stuff
+    int choice = 0;
+    int TaskCount = 0;
+    string ToDoList[100];
+    //end int//
+//---------------------------------------------------//
+    //String Array Setup//
+        for (int i = 0; i < 100; i++){
+        ToDoList[i] = " ";
+        }
+    //End String Array Setup//
+//---------------------------------------------------//
+    //start programs//
+    while (choice != 7) {
+        Menu();
+        cin >> choice;
+        switch (choice) {
+            case 1:
+                ToDoList[TaskCount] = AddTask(ToDoList, TaskCount);
+                TaskCount++;
+                break;
+            case 2:
+                DisplayTask(ToDoList, TaskCount);
+                break;
+            case 3:
+
+                break;
+            case 4:
+
+                break;
+            case 5:
+
+                break;
+            case 6:
+
+                break;
+            case 7:
+            cout << "Ending Program.\n";
+        }
+    }
+    return 0;
+}
+//---------------------------------------------------//
